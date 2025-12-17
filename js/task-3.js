4ef5793492d7eee35dd12e40756828745e47c14f
Andrii-Mitko <andreymit123@gmail.com>
2025-12-17 18:37:08 +0200

    return borderBox

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ function getElementWidth(content, padding, border) {
     Number.parseFloat(content) +
     Number.parseFloat(padding) * 2 +
     Number.parseFloat(border) * 2;
-  return `${borderBox}`;
+  return borderBox;
 }
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
